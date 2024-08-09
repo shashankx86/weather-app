a87227caea228495f21e708906c474ef6d4317c4
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-09 10:11:48 +0530

    Build functions to convert time and wind direction

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,43 @@
 // Utility functions for WeatherApp
 
-function convertTemp() {}
+function convertTime(hour) {
+    // Takes the hour in 24hr time and converts it to 12hr time with AM or PM
+    let newTime = "";
 
-function formatDate() {}
+    if (hour < 12) {
+        newTime = `${hour % 12 || 12}AM`;
+    } else {
+        newTime = `${hour % 12 || 12}PM`;
+    }
+
+    return newTime;
+}
+
+function convertWindDirection(deg) {
+    // Change in direction every 22.5 degrees
+    const val = Math.floor(deg / 22.5 + 0.5);
+    const compassDirections = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+    ];
+
+    const direction = compassDirections[val % 16];
+
+    return direction;
+}
+
+export { convertTime, convertWindDirection };
